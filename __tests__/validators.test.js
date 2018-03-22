3032ed770f8373e7f41b9dbc0e49b733cd67cfd6
MR Ngo <mrngo@thoughtworks.com>
2018-03-22 14:19:10 -0400

    start validation test file - MR

---
+++ __tests__/validators.test.js
@@ -0,0 +1,6 @@
+import { isValid, validForm, validNumFrames } from "../app/utilities/validators";
+import { endIsLaterThanStart, isValidInput, validateAllInputs } from "../app/utilities/validators";
+
+describe("Validator", () => {
+	expect(2).toBe(2);
+});
